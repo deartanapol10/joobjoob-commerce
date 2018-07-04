b45e3603fe55c9b7790f4c91067e0cd3619924e8
deartanapol10 <deartanapol10@gmail.com>
2018-07-04 22:01:48 +0700

    user authorization

--- routes/api/user.js
+++ routes/api/user.js
@@ -20,6 +20,8 @@ router.get("/test", (req, res) => {
 // @desc    Register user
 // @access  Public
 router.post("/register", (req, res) => {
+   const errors = {};
+
    const fields = {};
    if (req.body.username) fields.username = req.body.username;
    if (req.body.password) fields.password = req.body.password;
@@ -40,48 +42,122 @@ router.post("/register", (req, res) => {
    User.findOne({ username: req.body.username }).then(profile => {
       if (profile) {
          //User exist
-         res.status(400).json("Error : User exist");
+         errors.username = "Username exist";
+         res.status(400).json(errors);
       } else {
          //Create
-         new User(fields).save().then(profile => res.json(profile));
+         const newUser = new User(fields);
+         // .save()
+         // .then(profile => res.json(profile));
+
+         bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+               if (err) throw err;
+               newUser.password = hash;
+               newUser
+                  .save()
+                  .then(user => res.json(user))
+                  .catch(err => console.log(err));
+            });
+         });
       }
+   });
+});
+
+// @route   GET api/user/login
+// @desc    Login User / Returning JWT Token
+// @access  Public
+router.post("/login", (req, res) => {
+   const errors = {};
+
+   const username = req.body.username;
+   const password = req.body.password;
+
+   // Find user by email
+   User.findOne({
+      username
+   }).then(user => {
+      //Check for user
+      if (!user) {
+         errors.username = "User not found";
+         return res.status(404).json(errors);
+      }
+
+      //Check password
+      bcrypt.compare(password, user.password).then(isMatch => {
+         if (isMatch) {
+            //User matched
+
+            const payload = {
+               // Create jwt payload (put whatever you want into payload)
+               id: user.id
+               // name: user.name,
+               // avatar: user.avatar
+            };
+
+            //Sign token
+            jwt.sign(
+               payload,
+               keys.secretOrKey,
+               {
+                  expiresIn: 3600
+               },
+               (err, token) => {
+                  res.json({
+                     success: true,
+                     token: "Bearer " + token
+                  });
+               }
+            );
+         } else {
+            errors.password = "Password incorrect";
+            return res.status(400).json(errors);
+         }
+      });
    });
 });
 
 // @route   POST api/user/edit/:username
 // @desc    Edit user
-// @access  Public (should be private)
-router.post("/edit/:username", (req, res) => {
-   const fields = {};
-   if (req.body.username) fields.username = req.body.username;
-   if (req.body.password) fields.password = req.body.password;
-   if (req.body.email) fields.email = req.body.email;
-   if (req.body.firstname) fields.firstname = req.body.firstname;
-   if (req.body.lastname) fields.lastname = req.body.lastname;
+// @access  Private
+router.post(
+   "/edit/:username",
+   passport.authenticate("jwt", { session: false }),
+   (req, res) => {
+      const errors = {};
 
-   fields.store = {};
-   // if (req.body.storeId) fields.store.id = req.body.storeId;
-   // if (req.body.storeName) fields.store.name = req.body.storeName;
+      const fields = {};
+      if (req.body.username) fields.username = req.body.username;
+      if (req.body.password) fields.password = req.body.password;
+      if (req.body.email) fields.email = req.body.email;
+      if (req.body.firstname) fields.firstname = req.body.firstname;
+      if (req.body.lastname) fields.lastname = req.body.lastname;
 
-   if (req.body.address) fields.address = req.body.address;
-   if (req.body.phoneNumber) fields.phoneNumber = req.body.phoneNumber;
-   if (req.body.bankAccount) fields.bankAccount = req.body.bankAccount;
-   // fields.createdAt = Date.now();
-   fields.updatedAt = Date.now();
+      fields.store = {};
+      // if (req.body.storeId) fields.store.id = req.body.storeId;
+      // if (req.body.storeName) fields.store.name = req.body.storeName;
 
-   User.findOne({ username: req.params.username }).then(profile => {
-      if (profile) {
-         //Update
-         User.findOneAndUpdate(
-            { username: req.params.username },
-            { $set: fields },
-            { new: true }
-         ).then(profile => res.json(profile));
-      } else {
-         res.status(400).json("Error : No username");
-      }
-   });
-});
+      if (req.body.address) fields.address = req.body.address;
+      if (req.body.phoneNumber) fields.phoneNumber = req.body.phoneNumber;
+      if (req.body.bankAccount) fields.bankAccount = req.body.bankAccount;
+      // fields.createdAt = Date.now();
+      fields.updatedAt = Date.now();
+
+      User.findOne({ username: req.params.username }).then(profile => {
+         if (profile) {
+            //Update
+            User.findOneAndUpdate(
+               { username: req.params.username },
+               { $set: fields },
+               { new: true }
+            ).then(profile => res.json(profile));
+         } else {
+            errors.username = "No username";
+            res.status(400).json(errors);
+         }
+      });
+   }
+);
 
 // //Login form
 // router.get("/login", (req, res) => {
